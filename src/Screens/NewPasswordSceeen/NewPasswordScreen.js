d0cefab5f7345d029e4ea3d3ec561417711a2c50
CA-NAVEDK <naved.khan@crossasyst.com>
2023-10-10 23:56:30 +0530

    Authentication has been added in New password screen

--- src/Screens/NewPasswordSceeen/NewPasswordScreen.js
+++ src/Screens/NewPasswordSceeen/NewPasswordScreen.js
@@ -1,24 +1,37 @@
-import { View, Text, StyleSheet, ScrollView } from "react-native";
+import { View, Text, StyleSheet, ScrollView, Alert } from "react-native";
 import React, { useState } from "react";
 
 import CustomInput from "../../Components/CustomInput/CustomInput";
 import CustomButton from "../../Components/CustomButton/CustomButton";
-import SocialSignInButtons from "../../Components/SocialSignInButtons/SocialSignInButtons";
-import { useNavigation } from "@react-navigation/native";
+import { useNavigation, useRoute } from "@react-navigation/native";
 import { useForm } from "react-hook-form";
+import { Auth } from "aws-amplify";
 
 const NewPasswordScreen = () => {
-  const [code, setCode] = useState("");
-  const [newPassword, setNewPassword] = useState("");
+  const [loading, setLoading] = useState(false);
   const navigation = useNavigation();
+  const route = useRoute();
 
-  const {control, handleSubmit}= useForm();
+  const { control, handleSubmit, watch } = useForm({
+    defaultValues: { username: route?.params?.username },
+  });
+  const username = watch('username');
 
   const onSignInPressed = () => {
-    navigation.navigate('SignIn');
+    navigation.navigate("SignIn");
   };
-  const onSubmitPressed = () => {
-    navigation.navigate('Home')
+  const onSubmitPressed = async (data) => {
+    if (loading) {
+      return;
+    }
+    setLoading(true);
+    try {
+      await Auth.forgotPasswordSubmit(username, data.code, data.password);
+      navigation.navigate("Home");
+    } catch (e) {
+      Alert.alert("Oops", e.message);
+    }
+    setLoading(false);
   };
 
   return (
@@ -26,26 +39,58 @@ const NewPasswordScreen = () => {
       <View style={styles.root}>
         <Text style={styles.title}>Reset your Password</Text>
         <CustomInput
-          placeholder="Code"
-          name='code'
+          placeholder="Username"
+          name="username"
           control={control}
-          rules={{required: 'Code is required', minLength: {value:6, message:'Code should be 6 digit long'}, maxLength: {value:6, message:'Code should be 6 digit long'}}}
+          rules={{
+            required: "Username is required",
+            minLength: {
+              value: 3,
+              message: "Username should be minimum 3 characters long",
+            },
+            maxLength: {
+              value: 24,
+              message: "Username should be maximum 24 characters long",
+            },
+          }}
+        />
+        <CustomInput
+          placeholder="Code"
+          name="code"
+          control={control}
+          rules={{
+            required: "Code is required",
+            minLength: { value: 6, message: "Code should be 6 digit long" },
+            maxLength: { value: 6, message: "Code should be 6 digit long" },
+          }}
           secureTextEntry
         />
         <CustomInput
           placeholder="Enter your New Password"
-          name='new-password'
+          name="password"
           control={control}
-          rules={{required: 'New password is required', minLength: {value:6, message:'Code should be minimum 8 characters long'}, maxLength: {value:24, message:'Code should be maximum 24 characters long'}}}
+          rules={{
+            required: "New password is required",
+            minLength: {
+              value: 6,
+              message: "Code should be minimum 8 characters long",
+            },
+            maxLength: {
+              value: 24,
+              message: "Code should be maximum 24 characters long",
+            },
+          }}
           secureTextEntry
-
         />
-        <CustomButton text="Submit" onPress={handleSubmit(onSubmitPressed)} />
-          <CustomButton
-            text="Back to Sign In"
-            onPress={onSignInPressed}
-            type="TERTIARY"
-          />
+        <CustomButton
+          text={loading ? "Loading..." : "Submit"}
+          onPress={handleSubmit(onSubmitPressed)}
+        />
+        <CustomButton
+          text="Back to Sign In"
+          onPress={onSignInPressed}
+          type="TERTIARY"
+        />
       </View>
     </ScrollView>
   );
