d3ead1dd0e1f541cdcca1e4b371408211a71f1cc
CA-NAVEDK <naved.khan@crossasyst.com>
2023-10-10 22:59:02 +0530

    Authentication completed for Signup screen

--- src/Screens/SignUpScreen/SignUpScreen.js
+++ src/Screens/SignUpScreen/SignUpScreen.js
@@ -6,21 +6,35 @@ import CustomButton from "../../Components/CustomButton/CustomButton";
 import SocialSignInButtons from "../../Components/SocialSignInButtons/SocialSignInButtons";
 import { useNavigation } from "@react-navigation/native";
 import { useForm } from "react-hook-form";
+import { Auth } from "aws-amplify";
 
 const SignUpScreen = () => {
-  const [username, setUsername] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [repeatPassword, setRepeatPassword] = useState("");
+  const [loading, setLoading] = useState(false);
   const navigation = useNavigation();
 
   const { control, handleSubmit, watch } = useForm();
-  const pwd= watch('password');
-  const EMAIL_REGEX = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  const pwd = watch("password");
+  const EMAIL_REGEX =
+    /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
+  const OnRegisterPressed = async (data) => {
+    const { username, password, email, name } = data;
 
-  const OnRegisterPressed = () => {
-    navigation.navigate("ConfirmEmail");
+    if (loading) {
+      return;
+    }
+    setLoading(true);
+    try {
+      await Auth.signUp({
+        username,
+        password,
+        attributes: { email, name, preferred_username: username },
+      });
+      navigation.navigate("ConfirmEmail");
+    } catch (e) {
+      Alert.alert("Oops", e.message);
+    }
+    setLoading(false);
   };
   const onSignInPressed = () => {
     navigation.navigate("SignIn");
@@ -37,32 +51,76 @@ const SignUpScreen = () => {
       <View style={styles.root}>
         <Text style={styles.title}>Create an Account</Text>
         <CustomInput
+          placeholder="Name"
+          control={control}
+          name="name"
+          rules={{
+            required: "Name is required",
+            minLength: {
+              value: 3,
+              message: "Name should be minimum 3 characters long",
+            },
+            maxLength: {
+              value: 24,
+              message: "Name should be maximum 24 characters long",
+            },
+          }}
+        />
+        <CustomInput
           placeholder="Username"
           control={control}
-          name='username'
-          rules={{required: 'Username is required',  minLength: {value:3, message: 'Username should be minimum 3 characters long'}, maxLength: {value:8, message: 'Username should be maximum 24 characters long'}}}          
+          name="username"
+          rules={{
+            required: "Username is required",
+            minLength: {
+              value: 3,
+              message: "Username should be minimum 3 characters long",
+            },
+            maxLength: {
+              value: 24,
+              message: "Username should be maximum 24 characters long",
+            },
+          }}
         />
-        <CustomInput 
-        placeholder="Email" 
-        name='email'
-        control={control}
-        rules={{required:'Email is required', pattern:{value: EMAIL_REGEX, message:'Email is invalid'} }}
+        <CustomInput
+          placeholder="Email"
+          name="email"
+          control={control}
+          rules={{
+            required: "Email is required",
+            pattern: { value: EMAIL_REGEX, message: "Email is invalid" },
+          }}
         />
         <CustomInput
           placeholder="Password"
-          name='password'
+          name="password"
           control={control}
           secureTextEntry
-          rules={{required:'Password is required', minLength: {value:3, message: 'Password should be minimum 3 characters long'}, maxLength: {value:8, message: 'Password should be maximum 8 characters long'}}}
+          rules={{
+            required: "Password is required",
+            minLength: {
+              value: 3,
+              message: "Password should be minimum 3 characters long",
+            },
+            maxLength: {
+              value: 8,
+              message: "Password should be maximum 8 characters long",
+            },
+          }}
         />
         <CustomInput
           placeholder="password-repeat"
-          name='repeatPassword'
+          name="repeatPassword"
           control={control}
           secureTextEntry
-          rules={{validate: value => value ===pwd || 'Password do not match'}}
+          rules={{
+            validate: (value) => value === pwd || "Password do not match",
+          }}
         />
-        <CustomButton text="Register" onPress={handleSubmit(OnRegisterPressed)} />
+        <CustomButton
+          text={loading ? "Loading..." : "Register"}
+          onPress={handleSubmit(OnRegisterPressed)}
+        />
         <Text style={styles.text}>
           By registering, you confirm that you accept our{" "}
           <Text style={styles.link} onPress={onTermsOfUsePressed}>
